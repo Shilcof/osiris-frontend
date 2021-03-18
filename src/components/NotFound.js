36733de4c55c533588c32a79830a37d984cff3ca
Shilcof <robert.shilcof@gmail.com>
2021-03-18 11:16:09 +0000

    formats notfound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,13 @@
 const NotFound = (props) => {
     return (
         <div>
-            "Not Found"
+            <div className="text-center py-4">
+                <h1 style={{fontSize: 100}}>404</h1>
+            </div>
+            <div className="text-center py-4 my-4">
+                <h1 className="text-wrap text-break">Sorry the route {props.match.url} does not yet exist</h1>
+                <p className="lead">Follow the links in the navbar to start view listing</p>
+            </div>
         </div>
     )
 }
