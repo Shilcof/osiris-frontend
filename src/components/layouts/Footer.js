c3f8e0df6c5b3d66c02d29c2d220f6b4f81ca34b
Shilcof <robert.shilcof@gmail.com>
2021-03-19 14:54:19 +0000

    adds githiub link to footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,9 +1,12 @@
 const Footer = () => {
+
     return (
         <footer className="footer mt-auto">
             <div className="container text-center pb-3">
             <hr/>
-                <span className="text-muted small">Osiris established 2021</span>
+                <span className="text-muted small">
+                    Osiris established 2021 | <a href="https://github.com/Shilcof/osiris-frontend" >GitHub</a>
+                </span>
             </div>
         </footer>
     );
